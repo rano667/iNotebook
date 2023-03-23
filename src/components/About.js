51ca35828f49256c3bde24e532dd2e9aaec67936
rano667 <ranjanm667@gmail.com>
2023-03-23 21:35:26 +0530

    about details changed

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const About = () => {
-  if(1){
-    return <>ranjan</>
-  }
-  return <div>This is About</div>;
+  return (
+    <div>
+      <p>
+      The Build is done on MERN Stack. This is a CRUD Application. This ststic Page
+      requires it Backend Server and DB to run.
+      </p>
+      Thank You.
+    </div>
+  );
 };
 
 export default About;
