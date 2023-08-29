f481ab72b984e3a0bcb3348dc9036f957c78aa62
rano667 <71762466+rano667@users.noreply.github.com>
2023-08-29 17:55:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,17 @@ app.use(cors())
 
 //Available routes
 app.get('/', (req, res) => {
-  res.send('iNotebook');
+  res.json({
+    message : "iNotebook",
+    statusCode : 200
+  })
 })
 
 // /api/auth : The path for which the middleware function is invoked
 // require('./routes/auth') The Middle ware
 
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+// app.use('/api/auth',require('./routes/auth'))
+// app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port ${port}`)
