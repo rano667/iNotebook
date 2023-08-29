389e609b464de3e85e13eb0f427d8b050003f815
rano667 <71762466+rano667@users.noreply.github.com>
2023-08-29 18:47:28 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async ()=>{
-    await mongoose.connect('mongodb+srv://ranjan:ranjan@cluster0.gcryh.mongodb.net/inotebook')
-  .then(() => console.log('Mongo Connected!'));
+    try{
+           const url = 'mongodb+srv://ranjan:ranjan@cluster0.gcryh.mongodb.net/inotebook';
+        mongoose.set("strictQuery", false);
+       await mongoose.connect(url, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log("Database connection successfull");
+    }catch(error){
+        console.log("Error in database : ", error)
+    }
 }
 
 
