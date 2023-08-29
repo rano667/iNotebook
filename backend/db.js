d20df4b05ff90e3da863838af61d1d5e668bfbc5
rano667 <71762466+rano667@users.noreply.github.com>
2023-08-29 18:29:31 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async ()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/inotebook')
+    await mongoose.connect('mongodb+srv://ranjan:ranjan@cluster0.gcryh.mongodb.net/inotebook')
   .then(() => console.log('Mongo Connected!'));
 }
 
