f355681576019dfd23ec1a898e6c8e6606db3070
rano667 <71762466+rano667@users.noreply.github.com>
2023-08-29 18:54:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,20 @@ connectToMongo();
 // /api/auth : The path for which the middleware function is invoked
 // require('./routes/auth') The Middle ware
 
+
+
+//Route 1: Get all the notes using: GET "/api/notes/fetchallnotes". Login required.
+app.get("/fetchallnotes", async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("internal Server Error");
+  }
+});
+
+
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
